283e7e5f93cffff8e69052fdfc58f27afb386c1d
Ricardo Mendes Pereira Gomes <Ricardo.Mendes.Pereira.Gomes1@ibm.com>
2023-02-18 16:33:28 -0300

    adicionado lab e gitignore

---
+++ ansible-lab/db01/provision.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+cat <<EOT >> /home/vagrant/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDI+Q6/sSx6acYVxPfYtuOe1quQrVYS1vSjHHeTLWKGQmVfXzP0Xmqu8Ea7WFzMhjkumYy5YbUan0ec07Br5xjULhR5uA+Hu4HkuFnAX0u1wkpEifmmhAYgqn6UDiIyp9FyODnv8EJxnXf/FPjbnSWkmDl6xYcxKRrwAgHOc5uDVSAgJ38bICoECD9/n2Zwr+YwwyKy1ZkgSeNQJI6z+MTpaHvitf8fRGmzPtiqzmWyRYJY/27UDqMOyyr/QMsFx+jMVrRx3aUSAMTfsDmgpcQupDnu7+xpenFtxz/3uK84rfUaJlO0OIZpF2laN74EZ7UZbGM0iNrXY27cg52ZrijD vagrant@control-node
+EOT
